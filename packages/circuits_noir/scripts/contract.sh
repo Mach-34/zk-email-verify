1d6b2d6627ae2f4b902141e5ff862f20d6fbc95d
Jack Gilcrest <me@jp4g.llc>
2024-09-19 10:13:01 -0600

    update contract gen script

--- packages/circuits_noir/scripts/contract.sh
+++ packages/circuits_noir/scripts/contract.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-
+cd email-verifier-1024
 ## Write Verifier Key
-bb write_vk_ultra_honk -b ../target/noir_zkemail.json
+bb write_vk_ultra_honk -b ./target/noir_zkemail_1024.json -o ./target/vk_1024_honk
 ## Write Contract
-bb contract_ultra_honk -v ../target/vk
+bb contract_ultra_honk -v ./target/vk_1024_honk -o ./target/contract_1024_honk.sol
+
+bb write_vk -b ./target/noir_zkemail_1024.json -o ./target/vk_1024_plonk
+bb contract -v ./target/vk_1024_plonk -o ./target/contract_1024_plonk.sol
+cd - 
+# cd email-verifier-2048
+# bb write_vk_ultra_honk -b ../target/noir_zkemail_2048.json
+# ## Write Contract
+# bb contract_ultra_honk -v ../target/vk
