6b592298a1fc77be61a27134be341303a8ad0dd1
Jack Gilcrest <me@jp4g.llc>
2024-09-20 01:54:44 -0600

    new zkemail library

--- packages/circuits_noir/scripts/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd email-verifier-1024
-nargo compile --force
-cd ../email-verifier-2048
-nargo compile --force
-cd ..
--- packages/circuits_noir/scripts/contract.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-## Write Verifier Key
-bb write_vk_ultra_honk -b ../target/noir_zkemail.json
-## Write Contract
-bb contract_ultra_honk -v ../target/vk
